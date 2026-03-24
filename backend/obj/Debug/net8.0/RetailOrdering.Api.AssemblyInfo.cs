1027cd8383a21269f298fcb6d4ee2eb384e10be9
TejaSri128 <you@example.com>
2026-03-24 16:35:03 +0530

    adding updated files

--- backend/obj/Debug/net8.0/RetailOrdering.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RetailOrdering.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailOrdering.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a573253813d81ffd35a712893a740f85b4aa1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854269651434347e718cb3ed559c65fa786b49f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailOrdering.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailOrdering.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
